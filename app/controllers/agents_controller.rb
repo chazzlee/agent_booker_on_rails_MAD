0a7afa878b6d289126993e7fdc094103b514c2a9
chazzlee <l.chazz00@gmail.com>
2023-03-29 13:19:54 -0400

    cleanup

--- app/controllers/agents_controller.rb
+++ app/controllers/agents_controller.rb
@@ -6,19 +6,6 @@ class AgentsController < ApplicationController
 
   def show
     agent = Agent.preload(:timezone, :availabilities).find_by(id: params[:id])
-    # availabilities = group_availabilities(agent.availabilities)
     render :show, {locals: {agent:}}
-  end
-
-  private
-
-  def group_availabilities(availabilities)
-    availabilities.each_with_object({}) do |av, obj|
-      if obj[av.day_of_week].present?
-        obj[av.day_of_week].concat([av])
-      else
-        obj[av.day_of_week] = [av]
-      end
-    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,7 @@ Rails.application.routes.draw do
   resources :agents, only: [:index, :show] do
     resources :appointments, only: [:create]
   end
-
   get "/appointments/success", to: "appointments#show", as: "appointment_success"
 
-  # get "/agents", to: "agents#index"
-  # get "/agents/:id", to: "agents#show", as: "agent"
-
-  # post "/agents/:agent_id/appointments", to: "appointments#create", as: "agent_appointments"
   root "agents#index"
 end
